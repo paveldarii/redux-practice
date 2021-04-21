d222892eb82d304da076053498459b418694860d
Pavel Darii <paveldarii@yahoo.com>
2021-04-20 21:54:14 -0400

    added comment

--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -1,15 +1,16 @@
-import { configureStore, getDefaultMiddleware } from '@reduxjs/toolkit';
-import api from './middleware/api';
-import logger from './middleware/logger';
-import toast from './middleware/toastify';
-import reducer from './reducer';
+import { configureStore, getDefaultMiddleware } from "@reduxjs/toolkit";
+import api from "./middleware/api";
+import logger from "./middleware/logger";
+import toast from "./middleware/toastify";
+import reducer from "./reducer";
 
 export default function () {
+  //this middleware passes all request through it's check before going to the routes
   return configureStore({
     reducer,
     middleware: [
       ...getDefaultMiddleware(),
-      logger({ destination: 'console' }),
+      logger({ destination: "console" }),
       toast,
       api,
     ],
